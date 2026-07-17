0295ea49f71181b730b8dda2b590ca4250a6e68f
rams2state <ramstostate@gmail.com>
2026-07-16 21:24:45 -0500

    v3: price change indicators, price history graph, trainer-excluded rarity filter

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rarity-binder-v3';
+const CACHE_NAME = 'rarity-binder-v4';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './cards.csv'];
 
 self.addEventListener('install', (event) => {

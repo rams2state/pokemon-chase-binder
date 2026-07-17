d54728763e3d38ab468294fd91738cf9a258a90c
rams2state <ramstostate@gmail.com>
2026-07-16 22:30:19 -0500

    v5: pokeball icon, set symbols, radiant/crystal cards, mobile fixes, gainers/drops

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rarity-binder-v5';
+const CACHE_NAME = 'rarity-binder-v6';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './cards.csv'];
 
 self.addEventListener('install', (event) => {
